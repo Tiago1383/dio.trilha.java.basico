43c48ba840feb61f1b500d019066cdfcef285653
Tiago1383 <tiagotmelo_rs@hotmail.com>
2023-01-11 15:14:53 -0300

    Adicionada a classe operadores

---
+++ tipos-variaveis/src/operadores.java
@@ -0,0 +1,13 @@
+public class operadores {
+    
+    public static void main(String[] args){
+        int a, b;
+        a = 12;
+        b = 6;
+
+        int resultado = a % b;
+
+        System.out.println(resultado);
+
+    }
+}
