9624be70f0b17c7dd5d91e8caa0e6d63c2ea29bd
Tiago1383 <tiagotmelo_rs@hotmail.com>
2023-01-12 14:47:30 -0300

    Finalizando Métodos

--- tipos-variaveis/src/SmartTv.java
+++ tipos-variaveis/src/SmartTv.java
@@ -10,4 +10,26 @@ public class SmartTv {
     public void desligar(){
         ligada = false;
     }
+
+    public void aumentarVolume (){
+        volume ++;
+        System.out.println("Aumentando o volume para: " + volume);
+    }
+
+    public void diminuirVolume (){
+        volume --;
+        System.out.println("Diminuindo o volume para: " + volume);
+    }
+
+    public void mudarCanal(int novoCanal){
+        canal = novoCanal;
+    }
+
+    public void aumentarCanal(){
+        canal ++;
+    }
+
+    public void diminuirCanal(){
+        canal --;
+    }
 }
